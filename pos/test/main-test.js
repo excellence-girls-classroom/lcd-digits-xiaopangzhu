924f6628e66193bd8a88ac75fbd7542cf6d05854
xiaopanglzhu <1522898103@qq.com>
2016-04-21 18:05:28 +0800

    build jasmine

---
+++ pos/test/main-test.js
@@ -0,0 +1,9 @@
+describe('pos', function() {
+
+  it('should print correct text', function() {
+    var inputs = '邹子琴';
+
+    var expectText = '邹子琴';
+    expect(inputs).toEqual(expectText); 
+  });
+});
